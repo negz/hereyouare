22f51f25caef9a34648f87ba07f440458fbf2c76
Nic Cope <n+git@rk0n.org>
2013-07-11 21:07:55 +1000

    Add Facebook API module.

--- gkhereyouare.py
+++ gkhereyouare.py
@@ -1,5 +1,16 @@
+"""Polls and stores all Facebook checkins in a chosen area.
+
+Quick and dirty app to poll all Facebook checkins in the vicinity of a 
+chosen venue, and store them for reference.
+"""
+import facebook
 import webapp2
 
+EPICENTRE_PAGE='239927392813748'
+RADIUS='1500'
+
+# search?type=place&center=-33.897947275461,151.17869226555&distance=1500
+# 158480197527281/checkins
 
 class RootHandler(webapp2.RequestHandler):
   def get(self):
